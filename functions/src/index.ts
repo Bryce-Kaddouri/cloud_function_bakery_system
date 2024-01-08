dfb6e8eaa8dc15ad608b7342926de3abd90b08e5
Bryce-Kaddouri <bryce@MyCloudPAs-MacBook-Pro.local>
2024-01-08 17:40:52 +0000

    create user ok but verify token doesnt't work

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -17,24 +17,34 @@ export const createUser = functions.https.onRequest(async (req, res) => {
 
                 return res.status(403).json({error: "Unauthorized"});
             }
-            const token = req.headers.authorization.split("Bearer ")[1];
+            const idToken = req.headers.authorization.split("Bearer ")[1];
 
 
             // get email, password and role from request body
-            const {phone, password, role, fName, lName} = req.body;
-            console.log(token);
+            const {email, password, role, fName, lName} = req.body;
+            const isLogged = await
 
-            // check if token is valid
-            const decodedToken = await admin.auth().verifyIdToken("eyJhbGciOiJSUzI1NiIsImtpZCI6ImQxNjg5NDE1ZWMyM2EzMzdlMmJiYWE1ZTNlNjhiNjZkYzk5MzY4ODQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vY3VzdG9tLW9yZGVyLXN5c3RlbSIsImF1ZCI6ImN1c3RvbS1vcmRlci1zeXN0ZW0iLCJhdXRoX3RpbWUiOjE3MDQ3MjQ4MzAsInVzZXJfaWQiOiJsNFVLeFpjWFVYTmljUEh2WHJQaUZOYUNNZVkyIiwic3ViIjoibDRVS3haY1hVWE5pY1BIdlhyUGlGTmFDTWVZMiIsImlhdCI6MTcwNDcyNTc3NCwiZXhwIjoxNzA0NzI5Mzc0LCJlbWFpbCI6ImFkbWluQG1haWwuaWUiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsiYWRtaW5AbWFpbC5pZSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.HM2s1pDzI17HuO_l_JjPOHagC3AL6D2JgbypgbDzgD4SvWh1X4yzgSBeraUR2RZukHL0B0BLAQBjaa2LDOOucp_pFTi9A2ls-8ikmSQYTx1NVLpCE_fApKIRig48iUNvL-DkNKXZzG7FT6oCNAHrteFTzdBYIx4_kPTfQIE2tpWKxobg-wsgqD7HF8TwScbnmTk1buBpTsKsJpWKFzbmkEB7ViAWH4HGDklUfYXXtc3rGLaNJBtifCqeStYKNuArImv_8WwWYs_SY2NBgCB8oiGD0JjbKyX-LFmOr2nJ0vPVXvjlPsip8qJBIPAcfmCGd769fHFhMGtziMw_TskS2w");
-            if (!decodedToken) {
+                admin.auth()
+                .verifyIdToken(idToken, true);
+
+            if (!isLogged) {
                 return res.status(403).json({error: "Unauthorized"});
+            }else{
+                if (isLogged.customClaims.role !== "admin") {
+                    return res.status(403).json({error: "Unauthorized role"});
+                }
             }
 
-            if (!phone || !password || !role || !fName || !lName) {
+            // check if token is valid
+/*
+            const decodedToken = await admin.auth().verifyIdToken("eyJhbGciOiJSUzI1NiIsImtpZCI6ImQxNjg5NDE1ZWMyM2EzMzdlMmJiYWE1ZTNlNjhiNjZkYzk5MzY4ODQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vY3VzdG9tLW9yZGVyLXN5c3RlbSIsImF1ZCI6ImN1c3RvbS1vcmRlci1zeXN0ZW0iLCJhdXRoX3RpbWUiOjE3MDQ3MjQ4MzAsInVzZXJfaWQiOiJsNFVLeFpjWFVYTmljUEh2WHJQaUZOYUNNZVkyIiwic3ViIjoibDRVS3haY1hVWE5pY1BIdlhyUGlGTmFDTWVZMiIsImlhdCI6MTcwNDcyNTc3NCwiZXhwIjoxNzA0NzI5Mzc0LCJlbWFpbCI6ImFkbWluQG1haWwuaWUiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsiYWRtaW5AbWFpbC5pZSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.HM2s1pDzI17HuO_l_JjPOHagC3AL6D2JgbypgbDzgD4SvWh1X4yzgSBeraUR2RZukHL0B0BLAQBjaa2LDOOucp_pFTi9A2ls-8ikmSQYTx1NVLpCE_fApKIRig48iUNvL-DkNKXZzG7FT6oCNAHrteFTzdBYIx4_kPTfQIE2tpWKxobg-wsgqD7HF8TwScbnmTk1buBpTsKsJpWKFzbmkEB7ViAWH4HGDklUfYXXtc3rGLaNJBtifCqeStYKNuArImv_8WwWYs_SY2NBgCB8oiGD0JjbKyX-LFmOr2nJ0vPVXvjlPsip8qJBIPAcfmCGd769fHFhMGtziMw_TskS2w");
+*/
+
+            if (!email || !password || !role || !fName || !lName) {
                 const error = "Missing required fields:";
                 const errors = [];
-                if (!phone) {
-                    errors.push("phone");
+                if (!email) {
+                    errors.push("email");
                 }
                 if (!password) {
                     errors.push("password");
@@ -52,16 +62,20 @@ export const createUser = functions.https.onRequest(async (req, res) => {
 
             }else{
                 const user = await admin.auth().createUser({
-                    phoneNumber: phone,
+                    email: email,
                     password: password,
+                    displayName: fName + " " + lName,
                 });
+
+                await admin.auth().setCustomUserClaims(user.uid, {role: role});
+
                 await admin
                     .firestore()
                     .collection("users")
                     .doc(user.uid)
                     .set({
                         uid: user.uid,
-                        phone: phone,
+                        email: email,
                         role: role,
                         valid: true,
                     });
